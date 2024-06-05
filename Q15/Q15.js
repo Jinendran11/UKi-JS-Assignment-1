f5d6c2ca922a91b818b09ccafb0008e1ede0834a
Jinendran11 <mjinendran11@gmail.com>
2024-06-05 20:00:06 +0530

    Q15 js assignment

---
+++ Q15/Q15.js
@@ -0,0 +1,7 @@
+function updateEmployeeRecord(employee, updates) {
+    return { ...employee, ...updates }
+}
+
+const employee = { name: 'John', position: 'Developer', salary: 50000 }
+const updates = { position: 'Senior Developer', salary: 60000 }
+console.log(updateEmployeeRecord(employee, updates))
