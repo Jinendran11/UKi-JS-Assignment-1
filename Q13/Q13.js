b7b7a7e526d10f5a262185d626d132b6ff0ceef3
Jinendran11 <mjinendran11@gmail.com>
2024-06-05 19:42:33 +0530

    Q13 Js assignment

---
+++ Q13/Q13.js
@@ -0,0 +1,9 @@
+function addTask(tasks, newTask) 
+{
+    tasks.push(newTask)
+    return tasks
+}
+
+const tasks = ['Buy groceries', 'Clean the house']
+console.log(addTask(tasks, 'Pay bills' ))
+console.log(addTask(tasks, 'gaming' ))
